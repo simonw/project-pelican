a451f51f8d5d736eac8be61d5947e0b62487ec02
Simon Willison <swillison@gmail.com>
2021-07-14 16:18:56 -0700

    Use consistent secret between deploys
    This should stop deploys from logging everyone out by invalidating their cookies

--- publish.sh
+++ publish.sh
@@ -6,5 +6,6 @@ datasette publish cloudrun schools.db \
     -m metadata.yml \
     --template-dir templates \
     --service project-pelican \
+    --secret $DATASETTE_PUBLISH_SECRET \
     --extra-options "--setting trace_debug 1 --setting suggest_facets off --setting sql_time_limit_ms 10000 --setting facet_time_limit_ms 10000" \
     --memory 4Gi
