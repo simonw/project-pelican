8bed24bb99aabc2d9f617fd5967425f903051ffe
Simon Willison <swillison@gmail.com>
2021-07-13 21:37:55 -0700

    Run import in the right directory

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+pushd ~/data
 sqlite3 schools.db <<EOS
 .mode csv
 .import school.csv enrollments
 .import district.csv districts
-.import state.csv states
+.import data/state.csv states
 EOS
+popd
+mv ~/data/schools.db .
 sqlite-utils transform schools.db enrollments \
     --type county_state_id integer \
     --type district_nces_id integer \
