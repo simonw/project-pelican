bf6c090ddcd341584904606c90fc0916ed0824c2
Simon Willison <swillison@gmail.com>
2021-07-13 21:38:48 -0700

    Actually download the files

--- download.sh
+++ download.sh
@@ -7,5 +7,5 @@ do
     -H 'content-type: application/json' \
     --data-raw $'{"operationName":"CreateFileDownloadURI","variables":{"input":{"fileName":"'$filename$'","projectId":"UHJvamVjdDowMTFjZDQ5YS1iZDFkLTQ2NjEtYTU1OS1kODZkZDI1NTMyMWU="}},"query":"mutation CreateFileDownloadURI($input: FileURIInput\u0021) {\\n  createFileDownloadUri(input: $input) {\\n    ok {\\n      name\\n      uri\\n      __typename\\n    }\\n    err\\n    __typename\\n  }\\n}\\n"}' \
     --compressed | jq -r .data.createFileDownloadUri.ok.uri)
-  echo --key $filename -v $signed_url -o $download_dir/$filename
+  conditional-get --key $filename -v $signed_url -o $download_dir/$filename
 done
