9abbe4c6f561c94e547c55c60d09a8bbcccf5227
Simon Willison <swillison@gmail.com>
2021-07-20 18:25:34 -0700

    Extra _int columns for known almost-numeric columns
    Uses new feature from https://github.com/simonw/sqlite-transform/issues/13

--- build.sh
+++ build.sh
@@ -19,8 +19,19 @@ sqlite-utils transform schools.db enrollments \
     --type school_nces_id integer \
     --type school_state_id integer \
     --type year integer
+# create extra _int columns for known almost-numeric columns
+for column in white black hispanic native_american native_american_alaskan_native asian native_hawaiian asian_pacific_islander native_hawaiian_pacific_islander pacific_islander multiracial unknown_race male female non_binary unknown_gender ell homeless low_income free_and_reduced disability section_504 total
+do
+  sqlite-transform lambda schools.db enrollments $column \
+    --code '
+if value.isdigit() or value.replace(",", "").isdigit() or value.endswith(".0") or value.endswith(".5") or value.startswith("<"):
+    return int(float(value.replace(",", "").replace("<", "")))
+' --output "${column}_int" --output-type integer
+done
+# Create some indexes
 sqlite-utils create-index schools.db enrollments state
 sqlite-utils create-index schools.db enrollments school_nces_id
 sqlite-utils create-index schools.db enrollments district_nces_id
+# Enable full-text search
 sqlite-utils enable-fts schools.db enrollments school
 sqlite-utils vacuum schools.db
